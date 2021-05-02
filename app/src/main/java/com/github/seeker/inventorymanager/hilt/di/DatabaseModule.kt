7349e6dcca10a9fa3e41a3473d69360ab4eb0029
Nicholas Wright <dozedoffagain@gmail.com>
2021-05-02 17:33:14 +0200

    Create database instance via Hilt module

--- app/src/main/java/com/github/seeker/inventorymanager/hilt/di/DatabaseModule.kt
+++ app/src/main/java/com/github/seeker/inventorymanager/hilt/di/DatabaseModule.kt
@@ -1,12 +1,22 @@
 package com.github.seeker.inventorymanager.hilt.di
 
+import android.content.Context
+import androidx.room.Room
+import com.github.seeker.inventorymanager.persistence.InventoryDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object DatabaseModule {
 
-    //TODO Provide room database
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext applicationContext: Context): InventoryDatabase {
+        return Room.databaseBuilder(applicationContext, InventoryDatabase::class.java, "inventory-database").build();
+    }
 }
